ae2e4059bd99747df02a25108d954a6da67dbc9f
VJS <minsung_96@naver.com>
2022-02-07 23:57:03 +0900

    Sanity Queries

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -2,15 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 
 import { client } from '../client'
+import { feedQuery, searchQuery } from '../utils/data';
 import MasonryLayout from './MasonryLayout'
 import Spinner from './Spinner'
 
 const Feed = () => {
     const [loading, setLoading] = useState(false);
+    const { categoryId } = useParams()
+    const [pins, setPins] = useState(null);
+
+    useEffect(() => {
+        setLoading(true)
+
+        if(categoryId) {
+            const query = searchQuery(categoryId)
+
+            client.fetch(query)
+            .then((data) => {
+                setPins(data)
+                setLoading(false)
+            })
+        } else {
+            client.fetch(feedQuery)
+            .then((data) => {
+                setPins(data)
+                setLoading(false)
+            })
+        }
+    }, [categoryId])
+    
 
     if(loading) return <Spinner message="Loading your ideas..." />
     return (
-        <div></div>
+        <div>
+
+        </div>
     );
 };
 
