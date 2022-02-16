f6a6f4a224988719b0658d701aded8467ee67261
VJS <minsung_96@naver.com>
2022-02-16 23:59:43 +0900

    Profile #1

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+
+import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const UserProfile = () => {
+  const [user, setUser] = useState(null)
+  const [pins, setPins] = useState(null)
+  const [text, setText] = useState('Created')
+  const [activeButton, setActiveButton] = useState('Created')
+  const navigate = useNavigate()
+  const { userId } = useParams()
+
   return <div></div>;
 };
 
