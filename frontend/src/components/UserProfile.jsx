6328f76c2f6055a2a37b2a88ded666d8cb4d3628
VJS <minsung_96@naver.com>
2022-02-17 23:49:46 +0900

    Profile #2

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -8,6 +8,11 @@ import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
+const randomImage = 'https://source.unsplash.com/1600x900/?arts-culture,nature'
+
+const activeButtonStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none'
+const notActiveButtonStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none'
+
 const UserProfile = () => {
   const [user, setUser] = useState(null)
   const [pins, setPins] = useState(null)
@@ -16,7 +21,113 @@ const UserProfile = () => {
   const navigate = useNavigate()
   const { userId } = useParams()
 
-  return <div></div>;
+  useEffect(() => {
+    const query = userQuery(userId)
+
+    client.fetch(query)
+    .then((data) => {
+      setUser(data[0])
+    })
+  }, [userId])
+
+  useEffect(() => {
+    if(text === 'created') {
+      const createdPinsQuery = userCreatedPinsQuery(userId)
+      client.fetch(createdPinsQuery)
+      .then((data) => {
+        setPins(data)
+      })
+    } else {
+      const savedPinsQuery = userSavedPinsQuery(userId)
+      client.fetch(savedPinsQuery)
+      .then((data) => {
+        setPins(data)
+      })
+    }
+  }, [text, userId])
+
+  const logout = () => {
+    localStorage.clear()
+    navigate('/login')
+  }
+
+  if(!user) {
+    return <Spinner message="Loading Profile..." />
+  }
+  return (
+    <div className="relative pb-2 h-full justify-center items-center">
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-center items-center">
+            <img 
+              src={randomImage}
+              className="w-full h-370 2xl:h-510 shadow-lg object-cover"
+              alt="banner-pic"
+            />
+            <img 
+              src={user.image}
+              alt="user-pic"
+              className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover"
+            />
+            <h1 className="font-bold text-3xl text-center mt-3">
+              {user.userName}
+            </h1>
+            <div className="absolute top-0 z-1 right-0 p-2">
+              {userId === user._id && (
+                <GoogleLogout 
+                  clientId={process.env.REACT_APP_GOOGLE_API_ID}
+                  render={(renderProps) => (
+                      <button 
+                          type="button" 
+                          className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+                          onClick={renderProps.onClick}
+                          disabled={renderProps.disabled}
+                      >
+                        <AiOutlineLogout color="red" fontSize={21}/>
+                      </button>
+                  )}
+                  onLogoutSuccess={logout}
+                  cookiePolicy="single_host_origin"
+                />
+              )}
+            </div>
+          </div>
+          <div className="text-center mb-7">
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent)
+                setActiveButton('created')
+              }}
+              className={`${activeButton === 'created' ? activeButtonStyles : notActiveButtonStyles}`}
+            >
+              Created 
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent)
+                setActiveButton('saved')
+              }}
+              className={`${activeButton === 'saved' ? activeButtonStyles : notActiveButtonStyles}`}
+            >
+              Saved 
+            </button>
+          </div>
+          {pins?.length ? (
+            <div className="px-2">
+              <MasonryLayout pins={pins}/>
+            </div>
+          ) : (
+            <div className="flex justify-center font-bold items-center w-full text-xl mt-2">
+              No Pins Found.
+            </div>
+          )}
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
